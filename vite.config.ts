055266a2157ec424b66cf4ccd244b67e4e049602
Nagorik <67663664+Nagorik11@users.noreply.github.com>
2026-03-28 12:47:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
-
 export default defineConfig({
   build: {
-    sourcemap: false, // changed from inline to true
-    rollupOptions: {
-      output: {
-        sourcemap: false // added to generate separate .map files
-      }
-    }
-  }
+    sourcemap: false,
+  },
 });
