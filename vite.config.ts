478dbd5b6aeff27dde37af3fe16d4da3445d65c0
Nagorik <67663664+Nagorik11@users.noreply.github.com>
2026-03-28 12:51:03 -0300

    Update Vite config to include Cloudflare plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
+  plugins: [react(), cloudflare()],
   build: {
-    sourcemap: false,
-  },
+    sourcemap: false
+  }
 });
