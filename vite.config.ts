8b3e6d52ae564fbee2d9ca25744a6389c1245b5b
Nagorik <67663664+Nagorik11@users.noreply.github.com>
2026-03-28 12:06:51 -0300

    Disable sourcemap generation in Rollup output
    Changed sourcemap option from true to false in Rollup output.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     sourcemap: false, // changed from inline to true
     rollupOptions: {
       output: {
-        sourcemap: true // added to generate separate .map files
+        sourcemap: false // added to generate separate .map files
       }
     }
   }
